f0e9833de0a570d4bcccd09ef2febfc71bfb871e
skywarth <yigitk.ersoy@gmail.com>
2018-05-08 01:15:35 +0300

    Veritabanı dosyası v1.2
    Log ve operation tablosu eklendi

---
+++ databaseFolder/librarymanagement.sql
@@ -0,0 +1,385 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 07, 2018 at 11:58 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `librarymanagement`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `access_level`
+--
+
+CREATE TABLE `access_level` (
+  `access_level_id` int(11) NOT NULL,
+  `access_level_description` varchar(50) COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Dumping data for table `access_level`
+--
+
+INSERT INTO `access_level` (`access_level_id`, `access_level_description`) VALUES
+(1, 'admin'),
+(2, 'librarian'),
+(3, 'member');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `author`
+--
+
+CREATE TABLE `author` (
+  `author_id` int(11) NOT NULL,
+  `author_name` varchar(100) COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Dumping data for table `author`
+--
+
+INSERT INTO `author` (`author_id`, `author_name`) VALUES
+(1, 'Ahmet Kara'),
+(2, 'Hakan Can');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authors_of_book`
+--
+
+CREATE TABLE `authors_of_book` (
+  `authorsOfBook_id` int(11) NOT NULL,
+  `authorsOfBook_book_id` int(11) NOT NULL,
+  `authorsOfBook_author_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Dumping data for table `authors_of_book`
+--
+
+INSERT INTO `authors_of_book` (`authorsOfBook_id`, `authorsOfBook_book_id`, `authorsOfBook_author_id`) VALUES
+(1, 1, 1),
+(3, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `book_id` int(11) NOT NULL,
+  `book_title` varchar(100) COLLATE utf8_turkish_ci NOT NULL,
+  `book_number_of_pages` int(11) NOT NULL,
+  `book_category_id` int(11) NOT NULL,
+  `book_issue_status_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`book_id`, `book_title`, `book_number_of_pages`, `book_category_id`, `book_issue_status_id`) VALUES
+(1, 'Vadideki vakvaklar', 150, 2, 1),
+(2, 'uçan kaya', 222, 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(50) COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`category_id`, `category_name`) VALUES
+(2, 'Roman'),
+(3, 'Masal');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issue_status`
+--
+
+CREATE TABLE `issue_status` (
+  `issue_status_id` int(11) NOT NULL,
+  `issue_status_description` varchar(25) COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Dumping data for table `issue_status`
+--
+
+INSERT INTO `issue_status` (`issue_status_id`, `issue_status_description`) VALUES
+(1, 'not issued'),
+(2, 'issued');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `log`
+--
+
+CREATE TABLE `log` (
+  `log_id` int(11) NOT NULL,
+  `log_operation_id` int(11) NOT NULL,
+  `log_user_id` int(11) NOT NULL,
+  `log_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `operation`
+--
+
+CREATE TABLE `operation` (
+  `operation_id` int(11) NOT NULL,
+  `operation_description` varchar(25) COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Dumping data for table `operation`
+--
+
+INSERT INTO `operation` (`operation_id`, `operation_description`) VALUES
+(1, 'login'),
+(2, 'logout');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `return_status`
+--
+
+CREATE TABLE `return_status` (
+  `return_status_id` int(11) NOT NULL,
+  `return_status_description` varchar(25) COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Dumping data for table `return_status`
+--
+
+INSERT INTO `return_status` (`return_status_id`, `return_status_description`) VALUES
+(1, 'not returned'),
+(2, 'returned');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `transaction_id` int(11) NOT NULL,
+  `transaction_book_id` int(11) NOT NULL,
+  `transaction_member_id` int(11) NOT NULL,
+  `transaction_librarian_id` int(11) NOT NULL,
+  `transaction_date` date NOT NULL,
+  `transaction_return_status_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`transaction_id`, `transaction_book_id`, `transaction_member_id`, `transaction_librarian_id`, `transaction_date`, `transaction_return_status_id`) VALUES
+(1, 2, 4, 2, '2018-07-05', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_access_level_id` int(11) NOT NULL,
+  `user_username` varchar(25) COLLATE utf8_turkish_ci NOT NULL,
+  `user_password` varchar(25) COLLATE utf8_turkish_ci NOT NULL,
+  `user_name` varchar(100) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `user_tc` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_access_level_id`, `user_username`, `user_password`, `user_name`, `user_tc`) VALUES
+(1, 1, 'karamemet', '123456', 'Mustafa Kadir', 1567894561),
+(2, 2, 'niyazi', '123789', 'Niyazi Baba', 1479633215),
+(3, 3, 'tarkan', '753159', 'tarkan hilal', 1647651234),
+(4, 3, 'plaka', '44444', 'Ayşe fatma', 1231231233);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `access_level`
+--
+ALTER TABLE `access_level`
+  ADD PRIMARY KEY (`access_level_id`);
+
+--
+-- Indexes for table `author`
+--
+ALTER TABLE `author`
+  ADD PRIMARY KEY (`author_id`);
+
+--
+-- Indexes for table `authors_of_book`
+--
+ALTER TABLE `authors_of_book`
+  ADD PRIMARY KEY (`authorsOfBook_id`);
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`book_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `issue_status`
+--
+ALTER TABLE `issue_status`
+  ADD PRIMARY KEY (`issue_status_id`);
+
+--
+-- Indexes for table `log`
+--
+ALTER TABLE `log`
+  ADD PRIMARY KEY (`log_id`);
+
+--
+-- Indexes for table `operation`
+--
+ALTER TABLE `operation`
+  ADD PRIMARY KEY (`operation_id`);
+
+--
+-- Indexes for table `return_status`
+--
+ALTER TABLE `return_status`
+  ADD PRIMARY KEY (`return_status_id`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`transaction_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `access_level`
+--
+ALTER TABLE `access_level`
+  MODIFY `access_level_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `author`
+--
+ALTER TABLE `author`
+  MODIFY `author_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `authors_of_book`
+--
+ALTER TABLE `authors_of_book`
+  MODIFY `authorsOfBook_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `book`
+--
+ALTER TABLE `book`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `issue_status`
+--
+ALTER TABLE `issue_status`
+  MODIFY `issue_status_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `log`
+--
+ALTER TABLE `log`
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `operation`
+--
+ALTER TABLE `operation`
+  MODIFY `operation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `return_status`
+--
+ALTER TABLE `return_status`
+  MODIFY `return_status_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
