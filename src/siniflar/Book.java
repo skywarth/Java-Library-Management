cc0e47922a98cf42dcf80ab226107f226aebb015
skywarth <yigitk.ersoy@gmail.com>
2018-05-11 20:37:11 +0300

    siniflar v.14
    Book sınıfı güncellendi

--- src/siniflar/Book.java
+++ src/siniflar/Book.java
@@ -1,64 +1,85 @@
 package siniflar;
 
+import java.util.Date;
+
 public class Book {
 	private String author;
 	private int bookId;
 	private String category;
 	private int issueStatusId;
 	private int numberOfPages;
-	private  String releaseDate;
+	private  Date releaseDate;
 	private String title;
 	private IssueStatus issueStatus;
 	
-	public Book(String author,int bookId,String Category,int issueStatusId,int numberOfPages,String releaseDate,String title)
+	public Book(String author,int bookId,String category,int issueStatusId,int numberOfPages,Date releaseDate,String title)
 	{
-		this.issueStatus =new IssueStatus();
+	
 		this.author=author;
 		this.bookId=bookId;
 		this.category=category;
 		this.numberOfPages=numberOfPages;
 		this.releaseDate=releaseDate;
 		this.title=title;
-		issueStatus.issueStatusId(issueStatusId);
-	}
-	public Book()
-	{
-		this.author="--";
-		this.bookId=0;
-		this.category="--";
-		this.issueStatusId=0;
-		this.numberOfPages=0;
-		this.releaseDate="--";
-		this.title="--";
+		this.issueStatus.setIssueStatusId(issueStatusId);
 		
 	}
+
 	
 	public String getAuthor()
 	{
 		return this.author;
 	}
+	public void setAuthor(String author)
+	{
+		
+	}
 	public int getBookId()
 	{
 		return this.bookId;
+	}
+	public void setBookId(int bookId)
+	{
+		
 	}
 	public String getCategory()
 	{
 		return this.category;
 	}
+	public void setCategory(String category)
+	{
+		
+	}
 	public int getIssueStatusId()
 	{
 		return this.issueStatusId;
+	}
+	public void setIssueStatusId(int issueStatusId)
+	{
+		
 	}
 	public int getNumberOfPages()
 	{
 		return this.numberOfPages;
 	}
-	public  String getReleaseDate()
+	public void setNumberOfPages(int numberOfPages)
+	{
+	
+	}
+	public  Date getReleaseDate()
 	{
 		return this.releaseDate;
+	}
+	public  void setReleaseDate(String releaseDate)
+	{
+	
 	}
 	public String getTitle()
 	{
 		return this.title;
+	}
+	public void setTitle(String title)
+	{
+		
 	}
 }
