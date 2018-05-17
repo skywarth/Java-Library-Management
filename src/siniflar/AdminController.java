01821b4858dfdf71a4a2f2a5d9b3cded116f7ae5
skywarth <yigitk.ersoy@gmail.com>
2018-05-17 15:57:59 +0300

    siniflar v1.8
    abdüs + halil

--- src/siniflar/AdminController.java
+++ src/siniflar/AdminController.java
@@ -6,13 +6,13 @@ import javax.swing.table.DefaultTableModel;
 
 public interface AdminController 
 {
-	public void addLibrarian(int seviye, String userName,String password, String name, int tcNo, String mail, int phoneNumber, String address) throws SQLException;
-	public void addUser(int seviye, String userName,String password, String name, int tcNo, String mail, int phoneNumber, String address) throws SQLException;
+	public void add(int seviye, String userName,String password, String name, int tcNo, String mail, int phoneNumber, String address) throws SQLException;
+	
 	public void deletLibrarian(String userName, int tcNo, String name)throws SQLException;
-	public void deleteUser(String userName, int tcNo, String name) throws SQLException;
+	public void delete(int tcNo,int seviye) throws SQLException;
 	public void generateStatistic();
-	public DefaultTableModel getLibrarians(String query,String[] baslik) throws SQLException;
-	public DefaultTableModel getUsers(String query,String[] baslik)throws SQLException;
-	public String silBilgiGoster(String userName, int tcNo, String name)throws SQLException;
+	public DefaultTableModel get(String query,String[] baslik) throws SQLException;
+	
+	public String Bul(int tcNo,int seviye)throws SQLException;
 
 }
