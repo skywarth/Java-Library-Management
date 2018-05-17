649502502d806fea81bb9a684f3e76ce08bd111f
skywarth <yigitk.ersoy@gmail.com>
2018-05-17 16:34:04 +0300

    yonetici uye v1.3
    halil vers

--- src/YoneticiModul/YonetUye.java
+++ src/YoneticiModul/YonetUye.java
@@ -20,25 +20,31 @@ import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JTable;
 import java.awt.Font;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JPasswordField;
 
 public class YonetUye {
 
 	public JFrame frameUye;
 	private JTextField txtUyeTc;
 	private JTextField txtUyeKAdi;
-	private JTextField txtUyeParola;
 	private JTextField txtUyeAdi;
 	private JTextField txtUyeSoyadi;
-	private JTextField txtUyeDTarihi;
 	private JTextField txtUyeTelNo;
 	private JTextField txtUyeMail;
-	private JTextField txtUyelikTarihi;
 	private JTextField txtUyeGoruntuleTc;
 	private JTextField txtUyeSilTc;
 	private JTable tblUyeGoruntule;
-	private JTextField txtUyeSilKAdi;
-	private JTextField txtUyeSilAdi;
-	private JTextField txtUyeSilSoyadi;
+	private JTextField txtSilGkAdi;
+	private JTextField txtSilGTC;
+	private JTextField txtSilGAd;
+	private JTextField txtSilGMail;
+	private JTextField txtSilGSeviye;
+	private JTextField txtSilGSifre;
+	private JTextField txtSilGTelefon;
+	private JTextField txtSilGSoyad;
+	private JPasswordField txtUyeParola;
 
 	/**
 	 * Launch the application.
@@ -82,101 +88,73 @@ public class YonetUye {
 		panelUyeEkle.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("TC No :");
-		lblNewLabel.setBounds(24, 27, 89, 14);
+		lblNewLabel.setBounds(155, 45, 89, 14);
 		panelUyeEkle.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Ad\u0131 :");
-		lblNewLabel_1.setBounds(24, 102, 89, 14);
+		lblNewLabel_1.setBounds(155, 120, 89, 14);
 		panelUyeEkle.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Soyad\u0131 :");
-		lblNewLabel_2.setBounds(24, 127, 89, 14);
+		lblNewLabel_2.setBounds(155, 145, 89, 14);
 		panelUyeEkle.add(lblNewLabel_2);
 		
-		JLabel lblNewLabel_3 = new JLabel("Do\u011Fum Tarihi");
-		lblNewLabel_3.setBounds(24, 152, 89, 14);
-		panelUyeEkle.add(lblNewLabel_3);
-		
 		JLabel lblNewLabel_4 = new JLabel("Telefon :");
-		lblNewLabel_4.setBounds(24, 177, 89, 14);
+		lblNewLabel_4.setBounds(155, 170, 89, 14);
 		panelUyeEkle.add(lblNewLabel_4);
 		
 		JLabel lblNewLabel_5 = new JLabel("E-Mail :");
-		lblNewLabel_5.setBounds(24, 227, 89, 14);
+		lblNewLabel_5.setBounds(155, 192, 89, 14);
 		panelUyeEkle.add(lblNewLabel_5);
 		
-		JLabel lblNewLabel_6 = new JLabel("\u00DCyelik Tarihi :");
-		lblNewLabel_6.setBounds(24, 251, 89, 14);
-		panelUyeEkle.add(lblNewLabel_6);
-		
-		JLabel lblNewLabel_7 = new JLabel("Cinsiyet :");
-		lblNewLabel_7.setBounds(24, 202, 89, 14);
-		panelUyeEkle.add(lblNewLabel_7);
-		
 		JLabel lblNewLabel_8 = new JLabel("Adres :");
-		lblNewLabel_8.setBounds(24, 276, 89, 14);
+		lblNewLabel_8.setBounds(155, 217, 89, 14);
 		panelUyeEkle.add(lblNewLabel_8);
 		
 		JLabel lblKullancAd = new JLabel("Kullan\u0131c\u0131 Ad\u0131 :");
-		lblKullancAd.setBounds(24, 52, 89, 14);
+		lblKullancAd.setBounds(155, 70, 89, 14);
 		panelUyeEkle.add(lblKullancAd);
 		
 		JLabel lblParola = new JLabel("Parola :");
-		lblParola.setBounds(24, 77, 89, 14);
+		lblParola.setBounds(155, 95, 89, 14);
 		panelUyeEkle.add(lblParola);
 		
 		txtUyeTc = new JTextField();
-		txtUyeTc.setBounds(153, 24, 127, 20);
+		txtUyeTc.setBounds(284, 42, 127, 20);
 		panelUyeEkle.add(txtUyeTc);
 		txtUyeTc.setColumns(10);
 		
 		txtUyeKAdi = new JTextField();
-		txtUyeKAdi.setBounds(153, 49, 127, 20);
+		txtUyeKAdi.setBounds(284, 67, 127, 20);
 		panelUyeEkle.add(txtUyeKAdi);
 		txtUyeKAdi.setColumns(10);
 		
-		txtUyeParola = new JTextField();
-		txtUyeParola.setBounds(153, 74, 127, 20);
-		panelUyeEkle.add(txtUyeParola);
-		txtUyeParola.setColumns(10);
-		
 		txtUyeAdi = new JTextField();
-		txtUyeAdi.setBounds(153, 99, 127, 20);
+		txtUyeAdi.setBounds(284, 117, 127, 20);
 		panelUyeEkle.add(txtUyeAdi);
 		txtUyeAdi.setColumns(10);
 		
 		txtUyeSoyadi = new JTextField();
-		txtUyeSoyadi.setBounds(153, 124, 127, 20);
+		txtUyeSoyadi.setBounds(284, 142, 127, 20);
 		panelUyeEkle.add(txtUyeSoyadi);
 		txtUyeSoyadi.setColumns(10);
 		
-		txtUyeDTarihi = new JTextField();
-		txtUyeDTarihi.setBounds(153, 149, 127, 20);
-		panelUyeEkle.add(txtUyeDTarihi);
-		txtUyeDTarihi.setColumns(10);
-		
 		txtUyeTelNo = new JTextField();
-		txtUyeTelNo.setBounds(153, 174, 127, 20);
+		txtUyeTelNo.setBounds(284, 167, 127, 20);
 		panelUyeEkle.add(txtUyeTelNo);
 		txtUyeTelNo.setColumns(10);
 		
 		txtUyeMail = new JTextField();
-		txtUyeMail.setBounds(153, 224, 127, 20);
+		txtUyeMail.setBounds(284, 192, 127, 20);
 		panelUyeEkle.add(txtUyeMail);
 		txtUyeMail.setColumns(10);
 		
-		txtUyelikTarihi = new JTextField();
-		txtUyelikTarihi.setBounds(153, 248, 127, 20);
-		panelUyeEkle.add(txtUyelikTarihi);
-		txtUyelikTarihi.setColumns(10);
-		
-		JComboBox CboxUyeCinsiyet = new JComboBox();
-		CboxUyeCinsiyet.setModel(new DefaultComboBoxModel(new String[] {"K\u0131z", "Erkek"}));
-		CboxUyeCinsiyet.setBounds(153, 199, 59, 20);
-		panelUyeEkle.add(CboxUyeCinsiyet);
+		txtUyeParola = new JPasswordField();
+		txtUyeParola.setBounds(284, 92, 127, 20);
+		panelUyeEkle.add(txtUyeParola);
 		
 		TextArea txtUyeAdres = new TextArea();
-		txtUyeAdres.setBounds(153, 276, 284, 76);
+		txtUyeAdres.setBounds(284, 217, 284, 76);
 		panelUyeEkle.add(txtUyeAdres);
 		
 		JButton btnUyeEkle = new JButton("Ekle");
@@ -195,7 +173,7 @@ public class YonetUye {
 					String address=txtUyeAdres.getText();
 					//Ekleme(seviye, userName, password, name, tcNo, mail, phoneNumber, address);
 					Admin admin=new Admin();
-					admin.addUser(seviye, userName, password, name, tcNo, mail, phoneNumber, address);
+					admin.add(seviye, userName, password, name, tcNo, mail, phoneNumber, address);
 					JOptionPane.showMessageDialog(panelUyeEkle, "Ekleme Ýþlemi Baþarýlý");
 				}
 				catch (NumberFormatException ex) 
@@ -208,7 +186,7 @@ public class YonetUye {
 				}
 			}
 		});
-		btnUyeEkle.setBounds(348, 48, 89, 56);
+		btnUyeEkle.setBounds(479, 66, 89, 56);
 		panelUyeEkle.add(btnUyeEkle);
 		
 		JButton btnUyeCikis = new JButton("Temizle");
@@ -217,18 +195,17 @@ public class YonetUye {
 				txtUyeTc.setText("");
 				txtUyeAdi.setText("");
 				txtUyeKAdi.setText("");
-				txtUyeParola.setText("");
 				txtUyeSoyadi.setText("");
-				txtUyelikTarihi.setText("");
 				txtUyeMail.setText("");
 				txtUyeTelNo.setText("");
-				txtUyeDTarihi.setText("");
 				txtUyeAdres.setText(" ");
-				CboxUyeCinsiyet.setSelectedIndex(0);
+				
 			}
 		});
-		btnUyeCikis.setBounds(348, 123, 89, 56);
+		btnUyeCikis.setBounds(479, 141, 89, 56);
 		panelUyeEkle.add(btnUyeCikis);
+		
+		
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("Görüntüle", null, panel_1, null);
 		panel_1.setLayout(null);
@@ -243,8 +220,11 @@ public class YonetUye {
 		txtUyeGoruntuleTc.setColumns(10);
 		
 		tblUyeGoruntule = new JTable();
-		tblUyeGoruntule.setBounds(10, 39, 732, 306);
-		panel_1.add(tblUyeGoruntule);
+		tblUyeGoruntule.setBounds(10, 45, 732, 306);
+		
+		JScrollPane UyeSPane = new JScrollPane(tblUyeGoruntule);
+		UyeSPane.setBounds(10, 45, 732, 306);
+		panel_1.add(UyeSPane);
 		
 		JButton btnUyeGosterGun = new JButton("G\u00FCncelle");
 		btnUyeGosterGun.addActionListener(new ActionListener() {
@@ -252,11 +232,11 @@ public class YonetUye {
 			{
 				try
 				{
-					String []baslik={"1","2","3","4","5","6","7","8"};
+					String []baslik={"Eriþim Seviyesi","Kullanýcý Adý","Þifre","Ad-Soyad","TC NO","Mail","Telefon","Adres"};
 					String query="SELECT access_level.access_level_description,user.user_username,user.user_password,user.user_name,user.user_tc,user.user_email,user.user_phone_number,user.user_address FROM librarymanagement.user INNER JOIN librarymanagement.access_level ON user.user_access_level_id=access_level.access_level_id WHERE user.user_access_level_id='3'";
 		            //tblKuGoruntule.setModel(listele(query,baslik));
 					Admin admin=new Admin();
-					tblUyeGoruntule.setModel(admin.getUsers(query, baslik));
+					tblUyeGoruntule.setModel(admin.get(query, baslik));
 				}
 				catch (SQLException ex) 
 				{
@@ -264,58 +244,111 @@ public class YonetUye {
 				}
 			}
 		});
-		btnUyeGosterGun.setBounds(302, 7, 89, 23);
+		btnUyeGosterGun.setBounds(404, 7, 89, 23);
 		panel_1.add(btnUyeGosterGun);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(20, 45, 648, 306);
+		panel_1.add(scrollPane);
+		
+		JButton btnUyeBul = new JButton("Bul");
+		btnUyeBul.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0)
+			{
+				try
+				{
+					String []baslik={"Eriþim Seviyesi","Kullanýcý Adý","Þifre","Ad-Soyad","TC NO","Mail","Telefon","Adres"};
+					String query="SELECT access_level.access_level_description,user.user_username,user.user_password,user.user_name,user.user_tc,user.user_email,user.user_phone_number,user.user_address FROM librarymanagement.user INNER JOIN librarymanagement.access_level ON user.user_access_level_id=access_level.access_level_id WHERE user.user_access_level_id='3' AND user.user_tc='"+txtUyeGoruntuleTc.getText()+"'";
+		            //tblKuGoruntule.setModel(listele(query,baslik));
+					Admin admin=new Admin();
+					tblUyeGoruntule.setModel(admin.get(query, baslik));
+				}
+				catch (SQLException ex) 
+				{
+					ex.printStackTrace();
+				}
+			}
+		});
+		btnUyeBul.setBounds(305, 7, 89, 23);
+		panel_1.add(btnUyeBul);
+		
+		
 		
 		JPanel panelUyeSil = new JPanel();
 		tabbedPane.addTab("Silme", null, panelUyeSil, null);
 		panelUyeSil.setLayout(null);
 		
 		JLabel lblTcKimlikNumaras_1 = new JLabel("Tc Kimlik Numaras\u0131 :");
-		lblTcKimlikNumaras_1.setBounds(10, 38, 137, 14);
+		lblTcKimlikNumaras_1.setBounds(10, 19, 137, 14);
 		panelUyeSil.add(lblTcKimlikNumaras_1);
 		
 		txtUyeSilTc = new JTextField();
-		txtUyeSilTc.setBounds(157, 35, 100, 20);
+		txtUyeSilTc.setBounds(157, 16, 100, 20);
 		panelUyeSil.add(txtUyeSilTc);
 		txtUyeSilTc.setColumns(10);
-		
-		JLabel lblNewLabel_9 = new JLabel("Kulan\u0131c\u0131 Ad\u0131 :");
-		lblNewLabel_9.setBounds(10, 11, 137, 14);
-		panelUyeSil.add(lblNewLabel_9);
-		
-		txtUyeSilKAdi = new JTextField();
-		txtUyeSilKAdi.setBounds(157, 8, 100, 20);
-		panelUyeSil.add(txtUyeSilKAdi);
-		txtUyeSilKAdi.setColumns(10);
-		
-		JLabel lblNewLabel_10 = new JLabel("Ad\u0131 :");
-		lblNewLabel_10.setBounds(10, 66, 137, 14);
-		panelUyeSil.add(lblNewLabel_10);
-		
-		JLabel lblNewLabel_11 = new JLabel("Soyad\u0131 :");
-		lblNewLabel_11.setBounds(10, 91, 137, 14);
-		panelUyeSil.add(lblNewLabel_11);
-		
-		txtUyeSilAdi = new JTextField();
-		txtUyeSilAdi.setBounds(157, 63, 100, 20);
-		panelUyeSil.add(txtUyeSilAdi);
-		txtUyeSilAdi.setColumns(10);
-		
-		txtUyeSilSoyadi = new JTextField();
-		txtUyeSilSoyadi.setBounds(157, 91, 100, 20);
-		panelUyeSil.add(txtUyeSilSoyadi);
-		txtUyeSilSoyadi.setColumns(10);
 		
 		JLabel lblNewLabel_12 = new JLabel("Silinecek Ki\u015Fi Bilgileri");
 		lblNewLabel_12.setFont(new Font("Times New Roman", Font.BOLD, 14));
 		lblNewLabel_12.setBounds(422, 11, 212, 29);
 		panelUyeSil.add(lblNewLabel_12);
 		
-		JLabel lblUyeSilGoster = new JLabel("lblUyeSilGoster");
+		JLabel lblUyeSilGoster = new JLabel("");
 		lblUyeSilGoster.setFont(new Font("Times New Roman", Font.BOLD, 14));
 		lblUyeSilGoster.setBounds(340, 30, 380, 29);
 		panelUyeSil.add(lblUyeSilGoster);
+		
+		txtSilGkAdi = new JTextField();
+		txtSilGkAdi.setEditable(false);
+		txtSilGkAdi.setBounds(450, 88, 130, 20);
+		panelUyeSil.add(txtSilGkAdi);
+		txtSilGkAdi.setColumns(10);
+		
+		txtSilGTC = new JTextField();
+		txtSilGTC.setEditable(false);
+		txtSilGTC.setBounds(450, 190, 130, 20);
+		panelUyeSil.add(txtSilGTC);
+		txtSilGTC.setColumns(10);
+		
+		txtSilGAd = new JTextField();
+		txtSilGAd.setEditable(false);
+		txtSilGAd.setBounds(450, 138, 130, 20);
+		panelUyeSil.add(txtSilGAd);
+		txtSilGAd.setColumns(10);
+		
+		txtSilGMail = new JTextField();
+		txtSilGMail.setEditable(false);
+		txtSilGMail.setBounds(450, 215, 130, 20);
+		panelUyeSil.add(txtSilGMail);
+		txtSilGMail.setColumns(10);
+		
+		txtSilGSeviye = new JTextField();
+		txtSilGSeviye.setEditable(false);
+		txtSilGSeviye.setBounds(450, 63, 130, 20);
+		panelUyeSil.add(txtSilGSeviye);
+		txtSilGSeviye.setColumns(10);
+		
+		txtSilGSifre = new JTextField();
+		txtSilGSifre.setEditable(false);
+		txtSilGSifre.setBounds(450, 113, 130, 20);
+		panelUyeSil.add(txtSilGSifre);
+		txtSilGSifre.setColumns(10);
+		
+		txtSilGTelefon = new JTextField();
+		txtSilGTelefon.setEditable(false);
+		txtSilGTelefon.setBounds(450, 242, 130, 20);
+		panelUyeSil.add(txtSilGTelefon);
+		txtSilGTelefon.setColumns(10);
+		
+		txtSilGSoyad = new JTextField();
+		txtSilGSoyad.setEditable(false);
+		txtSilGSoyad.setBounds(450, 163, 130, 20);
+		panelUyeSil.add(txtSilGSoyad);
+		txtSilGSoyad.setColumns(10);
+		
+		JTextArea txtSilGAdres = new JTextArea();
+		txtSilGAdres.setEditable(false);
+		txtSilGAdres.setBounds(450, 273, 212, 78);
+		panelUyeSil.add(txtSilGAdres);
 		
 		JButton btnUyeSil = new JButton("S\u0130L");
 		btnUyeSil.addActionListener(new ActionListener() {
@@ -325,29 +358,41 @@ public class YonetUye {
 				{
 					Admin admin=new Admin();
 					int tcNo= Integer.valueOf(txtUyeSilTc.getText());
-					String userName=txtUyeSilKAdi.getText();
-					String name=txtUyeSilAdi.getText()+" "+txtUyeSilSoyadi.getText();
 					
-					//lblSilGoster.setText(silBilgiGoster(userName, tcNo, name));
-					lblUyeSilGoster.setText(admin.silBilgiGoster(userName, tcNo, name));
+					String bilgi= admin.Bul(tcNo, 3);
+					String[] bilgiler=bilgi.split(" ");
+					txtSilGSeviye.setText(bilgiler[0]);
+					txtSilGkAdi.setText(bilgiler[1]);
+					txtSilGSifre.setText(bilgiler[2]);
+					txtSilGAd.setText(bilgiler[3]);
+					txtSilGSoyad.setText(bilgiler[4]);
+					txtSilGTC.setText(bilgiler[5]);
+					txtSilGMail.setText(bilgiler[6]);
+					txtSilGTelefon.setText(bilgiler[7]);
+					txtSilGAdres.setText(bilgiler[8]);
+		
 					Object [] noDeleteOption = {"Evet","Hayýr","Ýptal" };
 					int noDelete=JOptionPane.showConfirmDialog(null, "Silmek Ýstiyor Musunuz ? ","Ýþlem Onayý!",JOptionPane.YES_NO_OPTION);
 					if(noDelete==JOptionPane.YES_OPTION)
 					{
 						//Silme(userName, tcNo, name);
-						admin.deletLibrarian(userName, tcNo, name);
+						admin.delete(3,tcNo);
 						txtUyeSilTc.setText("");
-						txtUyeSilKAdi.setText("");
-						txtUyeSilAdi.setText("");
-						txtUyeSilSoyadi.setText("");
+						txtUyeSilTc.setText("");
+						txtSilGSeviye.setText("");
+						txtSilGkAdi.setText("");
+						txtSilGSifre.setText("");
+						txtSilGAd.setText("");
+						txtSilGSoyad.setText("");
+						txtSilGTC.setText("");
+						txtSilGMail.setText("");
+						txtSilGTelefon.setText("");
+						txtSilGAdres.setText("");
 						lblUyeSilGoster.setText("SÝLME ÝÞLEMÝ BAÞARILI!");
 					}
 					else 
 					{
 						txtUyeSilTc.setText("");
-						txtUyeSilKAdi.setText("");
-						txtUyeSilAdi.setText("");
-						txtUyeSilSoyadi.setText("");
 					}	
 				}
 				catch (NumberFormatException ex) 
@@ -359,19 +404,74 @@ public class YonetUye {
 				}
 			}
 		});
-		btnUyeSil.setBounds(25, 151, 89, 23);
+		btnUyeSil.setBounds(26, 62, 89, 23);
 		panelUyeSil.add(btnUyeSil);
 		
 		JButton btnUyeSilVazgec = new JButton("Temizle");
 		btnUyeSilVazgec.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				lblUyeSilGoster.setText("");
 				txtUyeSilTc.setText("");
+				txtSilGSeviye.setText("");
+				txtSilGkAdi.setText("");
+				txtSilGSifre.setText("");
+				txtSilGAd.setText("");
+				txtSilGSoyad.setText("");
+				txtSilGTC.setText("");
+				txtSilGMail.setText("");
+				txtSilGTelefon.setText("");
+				txtSilGAdres.setText("");
 			}
 		});
-		btnUyeSilVazgec.setBounds(168, 151, 89, 23);
+		btnUyeSilVazgec.setBounds(157, 62, 89, 23);
 		panelUyeSil.add(btnUyeSilVazgec);
 		
+		JLabel lblKullancAd_1 = new JLabel("Erisim Seviyesi :");
+		lblKullancAd_1.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		lblKullancAd_1.setBounds(340, 65, 100, 17);
+		panelUyeSil.add(lblKullancAd_1);
 		
+		JLabel lblTcNo = new JLabel("Kullan\u0131c\u0131 Ad\u0131 :");
+		lblTcNo.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		lblTcNo.setBounds(340, 91, 111, 14);
+		panelUyeSil.add(lblTcNo);
+		
+		JLabel lblAd = new JLabel("\u015Eifre :");
+		lblAd.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		lblAd.setBounds(340, 116, 74, 14);
+		panelUyeSil.add(lblAd);
+		
+		JLabel lblMail = new JLabel("Ad :");
+		lblMail.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		lblMail.setBounds(340, 141, 74, 14);
+		panelUyeSil.add(lblMail);
+		
+		JLabel lblNewLabel_9 = new JLabel("TC No :");
+		lblNewLabel_9.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		lblNewLabel_9.setBounds(340, 193, 46, 14);
+		panelUyeSil.add(lblNewLabel_9);
+		
+		JLabel lblNewLabel_10 = new JLabel("Mail :");
+		lblNewLabel_10.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		lblNewLabel_10.setBounds(340, 218, 46, 14);
+		panelUyeSil.add(lblNewLabel_10);
+		
+		JLabel lblNewLabel_11 = new JLabel("Telefon :");
+		lblNewLabel_11.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		lblNewLabel_11.setBounds(340, 245, 74, 14);
+		panelUyeSil.add(lblNewLabel_11);
+		
+		JLabel lblNewLabel_13 = new JLabel("Adres :");
+		lblNewLabel_13.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		lblNewLabel_13.setBounds(340, 273, 46, 14);
+		panelUyeSil.add(lblNewLabel_13);
+		
+		JLabel lblSoyad = new JLabel("Soyad :");
+		lblSoyad.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		lblSoyad.setBounds(340, 166, 74, 14);
+		panelUyeSil.add(lblSoyad);
+	
 		frameUye.setBounds(100, 100, 793, 451);
 		frameUye.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frameUye.getContentPane().setLayout(null);
