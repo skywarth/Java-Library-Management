b441b9b8dcbb531cd1d75c570191c2dd94c48997
skywarth <yigitk.ersoy@gmail.com>
2018-05-17 16:29:03 +0300

    yonetici gui v1.2
    halil vers

--- src/YoneticiModul/YonetKutuphane.java
+++ src/YoneticiModul/YonetKutuphane.java
@@ -27,6 +27,7 @@ import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JTextArea;
 import java.awt.Font;
+import javax.swing.JPasswordField;
 
 public class YonetKutuphane {
 
@@ -36,17 +37,22 @@ public class YonetKutuphane {
 	private JTextField txtKutuphaneSoyadi;
 	private JTextField txtKutuphaneTel;
 	private JTextField txtKutuphaneKAdi;
-	private JTextField txtKutuphaneParola;
 	private JTextField txtKuGoruntuleTC;
 	private JTable tblKuGoruntule;
-	private JTextField txtKuSilTc;
 	private java.sql.Connection connection;
 	private Statement statement;
 	private ResultSet rs;
 	private JTextField txtKutuphaneMail;
-	private JTextField txtKuSilKAdi;
-	private JTextField txtKuSilAdi;
-	private JTextField txtKuSilSoyadi;
+	private JTextField txtSilBulTC;
+	private JTextField txtKAdi;
+	private JTextField txtTCNo;
+	private JTextField txtAd;
+	private JTextField txtMail;
+	private JTextField txtSeviye;
+	private JTextField txtSifre;
+	private JTextField txtTelefon;
+	private JTextField txtSoyad;
+	private JPasswordField txtKutuphaneParola;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -68,12 +74,12 @@ public class YonetKutuphane {
 	private void initialize() {
 		frameKutuphane = new JFrame();
 		frameKutuphane.setTitle("K\u00FCt\u00FCphane \u0130\u015Flemleri");
-		frameKutuphane.setBounds(100, 100, 712, 411);
+		frameKutuphane.setBounds(100, 100, 815, 479);
 		frameKutuphane.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frameKutuphane.getContentPane().setLayout(null);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(10, 11, 676, 351);
+		tabbedPane.setBounds(10, 11, 779, 419);
 		frameKutuphane.getContentPane().add(tabbedPane);
 		
 		JPanel pnlEkle = new JPanel();
@@ -81,54 +87,45 @@ public class YonetKutuphane {
 		pnlEkle.setLayout(null);
 		
 		JLabel lblTc = new JLabel("TC Numaras\u0131 :");
-		lblTc.setBounds(10, 11, 102, 14);
+		lblTc.setBounds(174, 44, 102, 14);
 		pnlEkle.add(lblTc);
 		
 		JLabel lblAd = new JLabel("Ad\u0131 :");
-		lblAd.setBounds(10, 92, 102, 14);
+		lblAd.setBounds(174, 125, 102, 14);
 		pnlEkle.add(lblAd);
 		
 		JLabel lblNewLabel = new JLabel("Soyad\u0131 :");
-		lblNewLabel.setBounds(10, 117, 102, 14);
+		lblNewLabel.setBounds(174, 150, 102, 14);
 		pnlEkle.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Telefon Numaras\u0131 :");
-		lblNewLabel_1.setBounds(10, 167, 133, 14);
+		lblNewLabel_1.setBounds(174, 175, 133, 14);
 		pnlEkle.add(lblNewLabel_1);
 		
-		JLabel lblCinsiyet = new JLabel("Cinsiyet :");
-		lblCinsiyet.setBounds(10, 142, 102, 14);
-		pnlEkle.add(lblCinsiyet);
-		
 		txtKutuphaneTCNo = new JTextField();
-		txtKutuphaneTCNo.setBounds(153, 8, 86, 20);
+		txtKutuphaneTCNo.setBounds(317, 41, 86, 20);
 		pnlEkle.add(txtKutuphaneTCNo);
 		txtKutuphaneTCNo.setColumns(10);
 		
 		txtKutuphaneAdi = new JTextField();
-		txtKutuphaneAdi.setBounds(153, 89, 86, 20);
+		txtKutuphaneAdi.setBounds(317, 122, 86, 20);
 		pnlEkle.add(txtKutuphaneAdi);
 		txtKutuphaneAdi.setColumns(10);
 		
 		txtKutuphaneSoyadi = new JTextField();
-		txtKutuphaneSoyadi.setBounds(153, 114, 86, 20);
+		txtKutuphaneSoyadi.setBounds(317, 147, 86, 20);
 		pnlEkle.add(txtKutuphaneSoyadi);
 		txtKutuphaneSoyadi.setColumns(10);
 		
 		txtKutuphaneTel = new JTextField();
-		txtKutuphaneTel.setBounds(153, 164, 86, 20);
+		txtKutuphaneTel.setBounds(317, 172, 86, 20);
 		pnlEkle.add(txtKutuphaneTel);
 		txtKutuphaneTel.setColumns(10);
 		
 		JTextArea txtKutuphaneAdres = new JTextArea();
 		txtKutuphaneAdres.setLineWrap(true);
-		txtKutuphaneAdres.setBounds(153, 220, 202, 92);
+		txtKutuphaneAdres.setBounds(317, 234, 202, 92);
 		pnlEkle.add(txtKutuphaneAdres);
-		
-		JComboBox cBoxCinsiyet = new JComboBox();
-		cBoxCinsiyet.setModel(new DefaultComboBoxModel(new String[] {"Erkek", "K\u0131z"}));
-		cBoxCinsiyet.setBounds(153, 138, 86, 22);
-		pnlEkle.add(cBoxCinsiyet);
 		
 		JButton btnKutuphaneKaydet = new JButton("EKLE");
 		btnKutuphaneKaydet.addActionListener(new ActionListener() {
@@ -147,7 +144,7 @@ public class YonetKutuphane {
 					String address=txtKutuphaneAdres.getText();
 					//Ekleme(seviye, userName, password, name, tcNo, mail, phoneNumber, address);
 					Admin admin=new Admin();
-					admin.addLibrarian(seviye, userName, password, name, tcNo, mail, phoneNumber, address);
+					admin.add(seviye, userName, password, name, tcNo, mail, phoneNumber, address);
 					JOptionPane.showMessageDialog(pnlEkle, "Ekleme Ýþlemi Baþarýlý");
 				}
 				catch (NumberFormatException ex) 
@@ -161,7 +158,7 @@ public class YonetKutuphane {
 				
 			}
 		});
-		btnKutuphaneKaydet.setBounds(300, 38, 89, 23);
+		btnKutuphaneKaydet.setBounds(464, 71, 89, 23);
 		pnlEkle.add(btnKutuphaneKaydet);
 		
 		JButton btnKutuphaneVazgec = new JButton("Temizle");
@@ -173,42 +170,40 @@ public class YonetKutuphane {
 				txtKutuphaneKAdi.setText("");
 				txtKutuphaneParola.setText("");
 				txtKutuphaneTel.setText("");
-				cBoxCinsiyet.setSelectedIndex(0);
 			}
 		});
-		btnKutuphaneVazgec.setBounds(300, 88, 89, 23);
+		btnKutuphaneVazgec.setBounds(464, 121, 89, 23);
 		pnlEkle.add(btnKutuphaneVazgec);
 		
 		JLabel lblKullancAd = new JLabel("Kullan\u0131c\u0131 Ad\u0131 :");
-		lblKullancAd.setBounds(10, 42, 102, 14);
+		lblKullancAd.setBounds(174, 75, 102, 14);
 		pnlEkle.add(lblKullancAd);
 		
 		txtKutuphaneKAdi = new JTextField();
-		txtKutuphaneKAdi.setBounds(153, 39, 86, 20);
+		txtKutuphaneKAdi.setBounds(317, 72, 86, 20);
 		pnlEkle.add(txtKutuphaneKAdi);
 		txtKutuphaneKAdi.setColumns(10);
 		
 		JLabel lblParola = new JLabel("Parola :");
-		lblParola.setBounds(10, 67, 102, 14);
+		lblParola.setBounds(174, 100, 102, 14);
 		pnlEkle.add(lblParola);
 		
-		txtKutuphaneParola = new JTextField();
-		txtKutuphaneParola.setBounds(153, 64, 86, 20);
-		pnlEkle.add(txtKutuphaneParola);
-		txtKutuphaneParola.setColumns(10);
-		
 		JLabel lblEmail = new JLabel("e-mail :");
-		lblEmail.setBounds(10, 192, 46, 14);
+		lblEmail.setBounds(174, 200, 46, 14);
 		pnlEkle.add(lblEmail);
 		
 		txtKutuphaneMail = new JTextField();
-		txtKutuphaneMail.setBounds(153, 189, 86, 20);
+		txtKutuphaneMail.setBounds(317, 203, 86, 20);
 		pnlEkle.add(txtKutuphaneMail);
 		txtKutuphaneMail.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Adres :");
-		lblNewLabel_2.setBounds(10, 217, 46, 14);
+		lblNewLabel_2.setBounds(174, 225, 46, 14);
 		pnlEkle.add(lblNewLabel_2);
+		
+		txtKutuphaneParola = new JPasswordField();
+		txtKutuphaneParola.setBounds(317, 97, 86, 20);
+		pnlEkle.add(txtKutuphaneParola);
 		
 		JPanel pnlGoruntule = new JPanel();
 		tabbedPane.addTab("Görüntüle", null, pnlGoruntule, null);
@@ -219,30 +214,30 @@ public class YonetKutuphane {
 		pnlGoruntule.add(lblTcNumaras);
 		
 		txtKuGoruntuleTC = new JTextField();
-		txtKuGoruntuleTC.setBounds(106, 8, 86, 20);
+		txtKuGoruntuleTC.setBounds(106, 8, 127, 20);
 		pnlGoruntule.add(txtKuGoruntuleTC);
 		txtKuGoruntuleTC.setColumns(10);
 		
 		tblKuGoruntule = new JTable();
 		tblKuGoruntule.setEnabled(false);
-		tblKuGoruntule.setBounds(10, 36, 651, 211);
-		pnlGoruntule.add(tblKuGoruntule);
+		tblKuGoruntule.setBounds(10, 36, 651, 276);
 		
-		JLabel lblGoster = new JLabel("goster");
-		lblGoster.setBounds(10, 263, 481, 14);
-		pnlGoruntule.add(lblGoster);
+		JScrollPane KutuphaneSPane = new JScrollPane(tblKuGoruntule);
+		KutuphaneSPane.setBounds(0, 36, 764, 344);
+		pnlGoruntule.add(KutuphaneSPane);
+		
 		
 		JButton btnGster = new JButton("G\u00FCncelle");
 		btnGster.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				try
 				{
-					String []baslik={"1","2","3","4","5","6","7","8"};
+					String []baslik={"Eriþim Seviyesi","Kullanýcý Adý","Þifre","Ad-Soyad","TC NO","Mail","Telefon","Adres"};
 					//String query="SELECT * FROM librarymanagement.user";
 					String query="SELECT access_level.access_level_description,user.user_username,user.user_password,user.user_name,user.user_tc,user.user_email,user.user_phone_number,user.user_address FROM librarymanagement.user INNER JOIN librarymanagement.access_level ON user.user_access_level_id=access_level.access_level_id WHERE user.user_access_level_id='2'";
 		            //tblKuGoruntule.setModel(listele(query,baslik));
 					Admin admin=new Admin();
-					tblKuGoruntule.setModel(admin.getLibrarians(query, baslik));
+					tblKuGoruntule.setModel(admin.get(query, baslik));
 				}
 				catch (SQLException e) 
 				{
@@ -250,90 +245,149 @@ public class YonetKutuphane {
 				}
 			}
 		});
-		btnGster.setBounds(202, 7, 89, 23);
+		btnGster.setBounds(343, 7, 89, 23);
 		pnlGoruntule.add(btnGster);
+		
+		JButton btnBul = new JButton("Bul");
+		btnBul.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				try
+				{
+					String []baslik={"Eriþim Seviyesi","Kullanýcý Adý","Þifre","Ad-Soyad","TC NO","Mail","Telefon","Adres"};
+					String query="SELECT access_level.access_level_description,user.user_username,user.user_password,user.user_name,user.user_tc,user.user_email,user.user_phone_number,user.user_address FROM librarymanagement.user INNER JOIN librarymanagement.access_level ON user.user_access_level_id=access_level.access_level_id WHERE user.user_access_level_id='2' AND user.user_tc='"+txtKuGoruntuleTC.getText()+"'";
+		            //tblKuGoruntule.setModel(listele(query,baslik));
+					Admin admin=new Admin();
+					tblKuGoruntule.setModel(admin.get(query, baslik));
+				}
+				catch (SQLException ex) 
+				{
+					ex.printStackTrace();
+				}
+			}
+		});
+		btnBul.setBounds(243, 7, 89, 23);
+		pnlGoruntule.add(btnBul);
+		
+		
 		
 		JPanel pnlSil = new JPanel();
 		tabbedPane.addTab("Silme", null, pnlSil, null);
 		pnlSil.setLayout(null);
 		
-		JLabel lblTcNo = new JLabel("TC Numaras\u0131 :");
-		lblTcNo.setBounds(10, 36, 103, 14);
-		pnlSil.add(lblTcNo);
+		JLabel label = new JLabel("Tc Kimlik Numaras\u0131 :");
+		label.setBounds(10, 19, 137, 14);
+		pnlSil.add(label);
 		
-		txtKuSilTc = new JTextField();
-		txtKuSilTc.setBounds(123, 33, 86, 20);
-		pnlSil.add(txtKuSilTc);
-		txtKuSilTc.setColumns(10);
+		txtSilBulTC = new JTextField();
+		txtSilBulTC.setColumns(10);
+		txtSilBulTC.setBounds(157, 16, 100, 20);
+		pnlSil.add(txtSilBulTC);
 		
-		JLabel lblNewLabel_3 = new JLabel("Kullan\u0131c\u0131 Ad\u0131 :");
-		lblNewLabel_3.setBounds(10, 11, 103, 14);
-		pnlSil.add(lblNewLabel_3);
+		JLabel label_1 = new JLabel("Silinecek Ki\u015Fi Bilgileri");
+		label_1.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		label_1.setBounds(422, 11, 212, 29);
+		pnlSil.add(label_1);
 		
-		txtKuSilKAdi = new JTextField();
-		txtKuSilKAdi.setBounds(123, 8, 86, 20);
-		pnlSil.add(txtKuSilKAdi);
-		txtKuSilKAdi.setColumns(10);
+		JLabel lblSilBil = new JLabel("");
+		lblSilBil.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		lblSilBil.setBounds(340, 30, 380, 29);
+		pnlSil.add(lblSilBil);
 		
-		JLabel lblAd_1 = new JLabel("Ad\u0131 :");
-		lblAd_1.setBounds(10, 61, 103, 14);
-		pnlSil.add(lblAd_1);
+		txtKAdi = new JTextField();
+		txtKAdi.setEditable(false);
+		txtKAdi.setColumns(10);
+		txtKAdi.setBounds(450, 88, 130, 20);
+		pnlSil.add(txtKAdi);
 		
-		JLabel lblSoyad = new JLabel("Soyad\u0131 :");
-		lblSoyad.setBounds(10, 89, 46, 14);
-		pnlSil.add(lblSoyad);
+		txtTCNo = new JTextField();
+		txtTCNo.setEditable(false);
+		txtTCNo.setColumns(10);
+		txtTCNo.setBounds(450, 190, 130, 20);
+		pnlSil.add(txtTCNo);
 		
-		txtKuSilAdi = new JTextField();
-		txtKuSilAdi.setBounds(123, 58, 86, 20);
-		pnlSil.add(txtKuSilAdi);
-		txtKuSilAdi.setColumns(10);
+		txtAd = new JTextField();
+		txtAd.setEditable(false);
+		txtAd.setColumns(10);
+		txtAd.setBounds(450, 138, 130, 20);
+		pnlSil.add(txtAd);
 		
-		txtKuSilSoyadi = new JTextField();
-		txtKuSilSoyadi.setBounds(123, 86, 86, 20);
-		pnlSil.add(txtKuSilSoyadi);
-		txtKuSilSoyadi.setColumns(10);
+		txtMail = new JTextField();
+		txtMail.setEditable(false);
+		txtMail.setColumns(10);
+		txtMail.setBounds(450, 215, 130, 20);
+		pnlSil.add(txtMail);
 		
-		JLabel lblSilBilgi = new JLabel("Silinecek Ki\u015Fi Bilgileri");
-		lblSilBilgi.setFont(new Font("Times New Roman", Font.BOLD, 14));
-		lblSilBilgi.setBounds(362, 11, 174, 23);
-		pnlSil.add(lblSilBilgi);
+		txtSeviye = new JTextField();
+		txtSeviye.setEditable(false);
+		txtSeviye.setColumns(10);
+		txtSeviye.setBounds(450, 63, 130, 20);
+		pnlSil.add(txtSeviye);
 		
-		JLabel lblSilGoster = new JLabel("lblSilBilgi");
-		lblSilGoster.setFont(new Font("Times New Roman", Font.BOLD, 14));
-		lblSilGoster.setBounds(250, 36, 411, 23);
-		pnlSil.add(lblSilGoster);
+		txtSifre = new JTextField();
+		txtSifre.setEditable(false);
+		txtSifre.setColumns(10);
+		txtSifre.setBounds(450, 113, 130, 20);
+		pnlSil.add(txtSifre);
 		
-		JButton btnKuSil = new JButton("S\u0130L");
-		btnKuSil.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) 
+		txtTelefon = new JTextField();
+		txtTelefon.setEditable(false);
+		txtTelefon.setColumns(10);
+		txtTelefon.setBounds(450, 242, 130, 20);
+		pnlSil.add(txtTelefon);
+		
+		txtSoyad = new JTextField();
+		txtSoyad.setEditable(false);
+		txtSoyad.setColumns(10);
+		txtSoyad.setBounds(450, 163, 130, 20);
+		pnlSil.add(txtSoyad);
+		
+		JTextArea txtAdres = new JTextArea();
+		txtAdres.setEditable(false);
+		txtAdres.setBounds(450, 273, 212, 78);
+		pnlSil.add(txtAdres);
+		
+		JButton btnSil = new JButton("S\u0130L");
+		btnSil.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
 			{
 				try
 				{
 					Admin admin=new Admin();
-					int tcNo= Integer.valueOf(txtKuSilTc.getText());
-					String userName=txtKuSilKAdi.getText();
-					String name=txtKuSilAdi.getText()+" "+txtKuSilSoyadi.getText();
+					int tcNo= Integer.valueOf(txtSilBulTC.getText());
 					
-					//lblSilGoster.setText(silBilgiGoster(userName, tcNo, name));
-					lblSilGoster.setText(admin.silBilgiGoster(userName, tcNo, name));
+					String bilgi= admin.Bul(tcNo, 2);
+					String[] bilgiler=bilgi.split(" ");
+					txtSeviye.setText(bilgiler[0]);
+					txtKAdi.setText(bilgiler[1]);
+					txtSifre.setText(bilgiler[2]);
+					txtAd.setText(bilgiler[3]);
+					txtSoyad.setText(bilgiler[4]);
+					txtTCNo.setText(bilgiler[5]);
+					txtMail.setText(bilgiler[6]);
+					txtTelefon.setText(bilgiler[7]);
+					txtAdres.setText(bilgiler[8]);
+					
 					Object [] noDeleteOption = {"Evet","Hayýr","Ýptal" };
 					int noDelete=JOptionPane.showConfirmDialog(null, "Silmek Ýstiyor Musunuz ? ","Ýþlem Onayý!",JOptionPane.YES_NO_OPTION);
 					if(noDelete==JOptionPane.YES_OPTION)
 					{
 						//Silme(userName, tcNo, name);
-						admin.deletLibrarian(userName, tcNo, name);
-						txtKuSilAdi.setText("");
-						txtKuSilSoyadi.setText("");
-						txtKuSilKAdi.setText("");
-						txtKuSilTc.setText("");
-						lblSilGoster.setText("SÝLME ÝÞLEMÝ BAÞARILI!");
+						admin.delete(2,tcNo);
+						txtSeviye.setText("");
+						txtKAdi.setText("");
+						txtSifre.setText("");
+						txtAd.setText("");
+						txtSoyad.setText("");
+						txtTCNo.setText("");
+						txtMail.setText("");
+						txtTelefon.setText("");
+						txtAdres.setText("");
+						lblSilBil.setText("SÝLME ÝÞLEMÝ BAÞARILI!");
 					}
 					else 
 					{
-						txtKuSilAdi.setText("");
-						txtKuSilSoyadi.setText("");
-						txtKuSilKAdi.setText("");
-						txtKuSilTc.setText("");
+						txtSilBulTC.setText("");
 					}	
 				}
 				catch (NumberFormatException ex) 
@@ -345,17 +399,73 @@ public class YonetKutuphane {
 				}
 			}
 		});
-		btnKuSil.setBounds(120, 132, 89, 23);
-		pnlSil.add(btnKuSil);
+		btnSil.setBounds(26, 62, 89, 23);
+		pnlSil.add(btnSil);
 		
-		JButton btnKuSilVazgec = new JButton("Temizle");
-		btnKuSilVazgec.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				txtKuSilTc.setText("");
+		JButton btnTemizle = new JButton("Temizle");
+		btnTemizle.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				txtSeviye.setText("");
+				txtKAdi.setText("");
+				txtSifre.setText("");
+				txtAd.setText("");
+				txtSoyad.setText("");
+				txtTCNo.setText("");
+				txtMail.setText("");
+				txtTelefon.setText("");
+				txtAdres.setText("");
+				lblSilBil.setText("");
+				txtSilBulTC.setText("");
 			}
 		});
-		btnKuSilVazgec.setBounds(273, 132, 89, 23);
-		pnlSil.add(btnKuSilVazgec);
+		btnTemizle.setBounds(157, 62, 89, 23);
+		pnlSil.add(btnTemizle);
+		
+		JLabel label_3 = new JLabel("Erisim Seviyesi :");
+		label_3.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		label_3.setBounds(340, 65, 100, 17);
+		pnlSil.add(label_3);
+		
+		JLabel label_4 = new JLabel("Kullan\u0131c\u0131 Ad\u0131 :");
+		label_4.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		label_4.setBounds(340, 91, 111, 14);
+		pnlSil.add(label_4);
+		
+		JLabel label_5 = new JLabel("\u015Eifre :");
+		label_5.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		label_5.setBounds(340, 116, 74, 14);
+		pnlSil.add(label_5);
+		
+		JLabel label_6 = new JLabel("Ad :");
+		label_6.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		label_6.setBounds(340, 141, 74, 14);
+		pnlSil.add(label_6);
+		
+		JLabel label_7 = new JLabel("TC No :");
+		label_7.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		label_7.setBounds(340, 193, 46, 14);
+		pnlSil.add(label_7);
+		
+		JLabel label_8 = new JLabel("Mail :");
+		label_8.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		label_8.setBounds(340, 218, 46, 14);
+		pnlSil.add(label_8);
+		
+		JLabel label_9 = new JLabel("Telefon :");
+		label_9.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		label_9.setBounds(340, 245, 74, 14);
+		pnlSil.add(label_9);
+		
+		JLabel label_10 = new JLabel("Adres :");
+		label_10.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		label_10.setBounds(340, 273, 46, 14);
+		pnlSil.add(label_10);
+		
+		JLabel label_11 = new JLabel("Soyad :");
+		label_11.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		label_11.setBounds(340, 166, 74, 14);
+		pnlSil.add(label_11);
 	}
 	
 /*	
