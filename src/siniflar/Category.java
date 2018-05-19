9cb626446915b7ce65ac62209069fb1b2f3af66d
skywarth <yigitk.ersoy@gmail.com>
2018-05-19 21:29:29 +0300

    siniflar v1.8
    halil vers

---
+++ src/siniflar/Category.java
@@ -0,0 +1,21 @@
+package siniflar;
+
+public class Category 
+{
+	private int ID;
+	private String name;
+	
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
